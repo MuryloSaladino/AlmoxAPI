f66e5481d558d2f12e58e9a776f5798edd6541e9
MuryloSaladino <murylosaladino@gmail.com>
2025-05-10 01:54:19 -0300

    fix: request repositories module

--- Almox.Persistence/Repository/Requests/RequestItemsRepository.cs
+++ Almox.Persistence/Repository/Requests/RequestItemsRepository.cs
@@ -1,2 +1,25 @@
+using Almox.Application.Repository.RequestItemsRepository;
+using Almox.Domain.Entities;
+using Almox.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace Almox.Persistence.Repository.Requests;
 
+public class RequestItemsRepository(
+    AlmoxContext almoxContext
+) : IRequestItemsRepository
+{
+    private readonly AlmoxContext context = almoxContext;
+    private readonly DbSet<RequestItem> dbSet = almoxContext.Set<RequestItem>();
+
+    public void Create(RequestItem requestItem)
+        => context.Add(requestItem);
+
+    public void Delete(Guid itemId, Guid requestId)
+    {
+        var entity = dbSet.Find(itemId, requestId);
+
+        if (entity is not null)
+            dbSet.Remove(entity);
+    }
+}
--- Almox.Persistence/Repository/Requests/RequestsRepository.cs
+++ Almox.Persistence/Repository/Requests/RequestsRepository.cs
@@ -1,25 +1,9 @@
-using Almox.Application.Repository.RequestItemsRepository;
+using Almox.Application.Repository.RequestsRepository;
 using Almox.Domain.Entities;
 using Almox.Persistence.Context;
-using Microsoft.EntityFrameworkCore;
 
 namespace Almox.Persistence.Repository.Requests;
 
-public class RequestItemsRepository(
+public class RequestsRepository(
     AlmoxContext almoxContext
-) : IRequestItemsRepository
-{
-    private readonly AlmoxContext context = almoxContext;
-    private readonly DbSet<RequestItem> dbSet = almoxContext.Set<RequestItem>();
-
-    public void Create(RequestItem requestItem)
-        => context.Add(requestItem);
-
-    public void Delete(Guid itemId, Guid requestId)
-    {
-        var entity = dbSet.Find(itemId, requestId);
-
-        if (entity is not null)
-            dbSet.Remove(entity);
-    }
-}
+) : BaseRepository<Request>(almoxContext), IRequestsRepository {}
