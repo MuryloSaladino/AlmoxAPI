b8e17406d089c7d7a640aa6bc04620c95e438361
MuryloSaladino <murylosaladino@gmail.com>
2025-05-10 01:54:39 -0300

    feat: repositories injection

--- Almox.Persistence/ServiceExtensions.cs
+++ Almox.Persistence/ServiceExtensions.cs
@@ -1,11 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Almox.Application.Config;
-using Almox.Domain.Repository;
-using Almox.Domain.Repository.UsersRepository;
 using Almox.Persistence.Context;
 using Almox.Persistence.Repository;
 using Almox.Persistence.Repository.Users;
+using Almox.Application.Repository;
+using Almox.Application.Repository.UsersRepository;
+using Almox.Application.Repository.ActionLogsRepository;
+using Almox.Persistence.Repository.ActionLogs;
+using Almox.Application.Repository.CategoriesRepository;
+using Almox.Persistence.Repository.Categories;
+using Almox.Persistence.Repository.Deliveries;
+using Almox.Application.Repository.DeliveriesRepository;
+using Almox.Application.Repository.DeliveryItemsRepository;
+using Almox.Application.Repository.DepartmentsRepository;
+using Almox.Persistence.Repository.Departments;
+using Almox.Application.Repository.ItemsRepository;
+using Almox.Persistence.Repository.Items;
+using Almox.Application.Repository.RequestsRepository;
+using Almox.Persistence.Repository.Requests;
+using Almox.Application.Repository.RequestItemsRepository;
 
 namespace Almox.Persistence;
 
@@ -18,6 +32,14 @@ public static class ServiceExtensions
         services.AddDbContext<AlmoxContext>(opt => opt.UseNpgsql(connection));
         services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+        services.AddScoped<IActionLogsRepository, ActionLogsRepository>();
+        services.AddScoped<ICategoriesRepository, CategoriesRepository>();
+        services.AddScoped<IDeliveriesRepository, DeliveriesRepository>();
+        services.AddScoped<IDeliveryItemsRepository, DeliveryItemsRepository>();
+        services.AddScoped<IDepartmentRepository, DepartmentsRepository>();
+        services.AddScoped<IItemsRepository, ItemsRepository>();
+        services.AddScoped<IRequestsRepository, RequestsRepository>();
+        services.AddScoped<IRequestItemsRepository, RequestItemsRepository>();
         services.AddScoped<IUsersRepository, UserRepository>();
     }
 }
