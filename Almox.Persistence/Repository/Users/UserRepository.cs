b6c525b4ab6dd99a58333d3b8b2a934bfca290ea
MuryloSaladino <murylosaladino@gmail.com>
2025-05-10 18:54:39 -0300

    feat: implementation for get users with filters in repository

--- Almox.Persistence/Repository/Users/UserRepository.cs
+++ Almox.Persistence/Repository/Users/UserRepository.cs
@@ -7,17 +7,19 @@ namespace Almox.Persistence.Repository.Users;
 
 public class UserRepository(
     AlmoxContext almoxContext
-) : BaseRepository<User>(almoxContext), 
-    IUsersRepository
+) : BaseRepository<User>(almoxContext), IUsersRepository
 {
     public Task<bool> ExistsByUsername(string username, CancellationToken cancellationToken)
-        => context
-            .Set<User>()
+        => context.Set<User>()
             .AnyAsync(user => user.Username == username, cancellationToken);
 
     public Task<User?> GetByUsername(string username, CancellationToken cancellationToken)
-        => context
-            .Set<User>()
+        => context.Set<User>()
             .FirstOrDefaultAsync(user => user.Username == username, cancellationToken);
 
+    public Task<List<User>> GetWithFilters(UsersQueryFilters filters, CancellationToken cancellationToken)
+        => context.Set<User>()
+            .Where(u => filters.Username == null || EF.Functions.ILike(u.Username, $"%{filters.Username}%"))
+            .Where(u => filters.Email == null || EF.Functions.ILike(u.Email, $"%{filters.Email}%"))
+            .ToListAsync(cancellationToken);
 }
