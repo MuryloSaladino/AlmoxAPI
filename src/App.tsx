42fbfaccbaca8b5bb35bd4362ba05aeee426a1bd
MuryloSaladino <murylosaladino@gmail.com>
2025-05-15 00:17:59 -0300

    feat: global app root

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+import { Outlet } from 'react-router';
+import { theme } from '@/config/mantine/theme';
+
+
 export function App() {
-    return(
-        <></>
+    return (
+        <MantineProvider theme={theme}>
+            <Outlet/>
+        </MantineProvider>
     )
 }
