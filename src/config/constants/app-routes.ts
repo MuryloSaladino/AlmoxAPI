185e1cea30de6eb5357a080173dd31261bd52a40
MuryloSaladino <murylosaladino@gmail.com>
2025-05-15 00:19:03 -0300

    feat: starter app routes

---
+++ src/config/constants/app-routes.ts
@@ -0,0 +1,9 @@
+export const AppRoutes = {
+    ROOT: "/",
+    LOGIN: "/login",
+
+    CATALOG: "/catalog",
+    CART: "/cart",
+    REQUESTS: "/requests",
+    REQUEST_DETAILS: "/requests/:requestId",
+} as const;
