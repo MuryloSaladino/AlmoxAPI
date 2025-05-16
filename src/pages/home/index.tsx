120c7214e5dd6ff6846e06c226d09a5595859fd0
MuryloSaladino <murylosaladino@gmail.com>
2025-05-16 11:13:53 -0300

    feat: home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,25 @@
+import { AppRoutes } from "@/config/constants/app-routes";
+import { Button, Center, Group, Stack, TextInput, Title } from "@mantine/core";
+import { IconSearch } from "@tabler/icons-react";
+
+export function Home() {
+    return (
+        <Center h="80%">
+            <Stack align="center" gap="lg">
+                <Group>
+                    <Title order={1}>Search items in our catalog</Title>
+                    <IconSearch size={58}/>
+                </Group>
+
+                <form action={AppRoutes.CATALOG}>
+                    <Group>
+                        <TextInput name="q"/>
+                        <Button type="submit">
+                            <IconSearch/>
+                        </Button>
+                    </Group>
+                </form>
+            </Stack>
+        </Center>
+    )
+}
