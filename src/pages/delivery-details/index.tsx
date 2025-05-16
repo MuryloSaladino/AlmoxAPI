9399551d12341a16be31e07c48958bb82d6767ff
MuryloSaladino <murylosaladino@gmail.com>
2025-05-16 11:23:21 -0300

    feat: app routes mapping

---
+++ src/pages/delivery-details/index.tsx
@@ -0,0 +1,7 @@
+export function DeliveryDetails() {
+    return (
+        <>
+        
+        </>
+    )
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,14 @@ import { App } from "@/App";
 import { AppLayout } from "@/components/app-layout";
 import { RouteProtection } from "@/components/route-protection";
 import { AppRoutes } from "@/config/constants/app-routes";
+import { Cart } from "@/pages/cart";
+import { Catalog } from "@/pages/catalog";
+import { Deliveries } from "@/pages/deliveries";
+import { DeliveryDetails } from "@/pages/delivery-details";
 import { Home } from "@/pages/home";
 import { Login } from "@/pages/login";
+import { RequestDetails } from "@/pages/request-details";
+import { Requests } from "@/pages/requests";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([{
@@ -22,6 +28,30 @@ export const router = createBrowserRouter([{
                         path: AppRoutes.ROOT,
                         Component: Home,
                     },
+                    {
+                        path: AppRoutes.CART,
+                        Component: Cart,
+                    },
+                    {
+                        path: AppRoutes.CATALOG,
+                        Component: Catalog,
+                    },
+                    {
+                        path: AppRoutes.DELIVERIES,
+                        Component: Deliveries,
+                    },
+                    {
+                        path: AppRoutes.DELIVERY_DETAILS(),
+                        Component: DeliveryDetails,
+                    },
+                    {
+                        path: AppRoutes.REQUESTS,
+                        Component: Requests,
+                    },
+                    {
+                        path: AppRoutes.REQUEST_DETAILS(),
+                        Component: RequestDetails,
+                    }
                 ]
             }]
         },
