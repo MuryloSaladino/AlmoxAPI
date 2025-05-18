495c18a89e057704f7f636bc2420a69f3e0fa7c3
MuryloSaladino <murylosaladino@gmail.com>
2025-05-17 23:43:13 -0300

    feat: history for delivery and request

---
+++ Almox.Domain/Entities/DeliveryHistory.cs
@@ -0,0 +1,14 @@
+using Almox.Domain.Common.Enums;
+
+namespace Almox.Domain.Entities;
+
+public class DeliveryHistory : BaseEntity
+{
+    public required Delivery Delivery { get; set; }
+    public required Guid DeliveryId { get; set; }
+
+    public required User UpdatedBy { get; set; }
+    public required Guid UpdatedById { get; set; }
+
+    public required DeliveryStatus Status { get; set; }
+}
