d8f8bafa9e5d6b6956da046f3af340a66eb824f9
MuryloSaladino <murylosaladino@gmail.com>
2025-05-23 16:33:23 -0300

    feat: current order in user context to serve as cart

--- src/providers/user.context.tsx
+++ src/providers/user.context.tsx
@@ -1,10 +1,14 @@
 import type { ParentComponent } from "@/types/utils/component-utils.types";
 import type { User } from "@/types/entities/user.types";
 import { createContext, useState } from "react";
+import type { Order } from "@/types/entities/orders.types";
 
 export interface IUserContext {
     user: User | null;
     updateUser: (user: User | null) => void;
+
+    ongoingOrder: Order | null;
+    updateOngoingOrder: (user: Order | null) => void;
 }
 
 export const UserContext = createContext({} as IUserContext);
@@ -12,8 +16,15 @@ export const UserContext = createContext({} as IUserContext);
 export function UserContextProvider(props: ParentComponent) {
 
     const [user, setUser] = useState<User | null>(null);
+    const [ongoingOrder, setOngoingOrder] = useState<Order | null>(null);
 
     const updateUser = (user: User | null) => setUser(user);
+    const updateOngoingOrder = (order: Order | null) => setOngoingOrder(order);
 
-    return <UserContext.Provider value={{ user, updateUser }} {...props}/>
+    return <UserContext.Provider {...props} value={{ 
+        user, 
+        updateUser, 
+        ongoingOrder,
+        updateOngoingOrder, 
+    }}/>
 }
