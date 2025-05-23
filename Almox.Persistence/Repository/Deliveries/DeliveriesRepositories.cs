9a4f1f5d0f232552777fed5346c9fa8ec50559a7
MuryloSaladino <murylosaladino@gmail.com>
2025-05-23 16:20:45 -0300

    feat: implementation for find deliveries with filters

--- Almox.Persistence/Repository/Deliveries/DeliveriesRepositories.cs
+++ Almox.Persistence/Repository/Deliveries/DeliveriesRepositories.cs
@@ -1,9 +1,29 @@
 using Almox.Application.Repository.DeliveriesRepository;
+using Almox.Domain.Common.Enums;
 using Almox.Domain.Entities;
 using Almox.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Almox.Persistence.Repository.Deliveries;
 
 public class DeliveriesRepository(
     AlmoxContext almoxContext
-) : BaseRepository<Delivery>(almoxContext), IDeliveriesRepository {}
+) : BaseRepository<Delivery>(almoxContext), IDeliveriesRepository
+{
+    public Task<List<Delivery>> GetWithFilters(
+        DeliveriesQueryFilters filters, CancellationToken cancellationToken)
+    {
+        var query = context.Set<Delivery>()
+            .Where(d => d.DeletedAt == null)
+            .AsQueryable();
+
+        if (filters.Status is DeliveryStatus statusFilter)
+            query = query.Where(d => d.Status == statusFilter);
+        if (filters.Start is DateTime startDateFilter)
+            query = query.Where(d => d.Date > startDateFilter);
+        if (filters.End is DateTime endDateFilter)
+            query = query.Where(d => d.Date < endDateFilter);
+
+        return query.ToListAsync(cancellationToken);
+    }
+}
