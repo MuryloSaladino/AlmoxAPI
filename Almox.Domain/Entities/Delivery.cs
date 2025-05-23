1bdc9dde648ec13c0eb58f1d76d81cde60e4aa4b
MuryloSaladino <murylosaladino@gmail.com>
2025-05-23 16:13:28 -0300

    refact: default value for delivery status

--- Almox.Domain/Entities/Delivery.cs
+++ Almox.Domain/Entities/Delivery.cs
@@ -11,5 +11,5 @@ public class Delivery : BaseEntity
 
     public string? Observations { get; set; } = null;
     public required DateTime Date { get; set; }
-    public DeliveryStatus Status { get; set; } = DeliveryStatus.Draft;
+    public DeliveryStatus Status { get; set; } = DeliveryStatus.Booked;
 }
