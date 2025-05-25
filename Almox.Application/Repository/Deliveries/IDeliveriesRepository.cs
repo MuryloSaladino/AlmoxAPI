8b8504e4f942446012427c0c81c61fb8edc79de7
MuryloSaladino <murylosaladino@gmail.com>
2025-05-25 16:01:33 -0300

    feat: filters for deliveries

--- Almox.Application/Repository/Deliveries/IDeliveriesRepository.cs
+++ Almox.Application/Repository/Deliveries/IDeliveriesRepository.cs
@@ -1,10 +1,13 @@
+using Almox.Domain.Common.Enums;
 using Almox.Domain.Entities;
 
 namespace Almox.Application.Repository.Deliveries;
 
+public record DeliveryFilters(
+    DeliveryStatus? Status,
+    DateTime? Start,
+    DateTime? End
+);
+
 public interface IDeliveriesRepository
-    : IBaseRepository<Delivery>
-{
-    Task<List<Delivery>> GetWithFilters(
-        DeliveriesQueryFilters filters, CancellationToken cancellationToken);       
-}
+    : IBaseRepository<Delivery, DeliveryFilters>;
