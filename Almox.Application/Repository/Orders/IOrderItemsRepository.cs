dd10e7d3a4bd29064f4db357b0e3d60b206832fa
MuryloSaladino <murylosaladino@gmail.com>
2025-05-25 16:02:53 -0300

    feat: concise repository for order items

--- Almox.Application/Repository/Orders/IOrderItemsRepository.cs
+++ Almox.Application/Repository/Orders/IOrderItemsRepository.cs
@@ -6,5 +6,4 @@ public interface IOrderItemsRepository
 {
     void Create(OrderItem orderItem);
     void Delete(Guid itemId, Guid orderId);
-    Task<OrderItem?> Get(Guid itemId, Guid orderId, CancellationToken cancellationToken);
 }
