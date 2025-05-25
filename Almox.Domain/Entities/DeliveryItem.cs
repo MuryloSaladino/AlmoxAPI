f1e799d3fa35bd00b9a97e8c2786888992b4427f
MuryloSaladino <murylosaladino@gmail.com>
2025-05-25 15:26:45 -0300

    feat: supplier price for delivery item

--- Almox.Domain/Entities/DeliveryItem.cs
+++ Almox.Domain/Entities/DeliveryItem.cs
@@ -2,11 +2,10 @@ namespace Almox.Domain.Entities;
 
 public class DeliveryItem
 {
+    public required Guid DeliveryId { get; set; }
     public required Guid ItemId { get; set; }
     public required Item Item { get; set; }
-
-    public required Guid DeliveryId { get; set; }
-    public required Delivery Delivery { get; set; }
     
     public required int Quantity { get; set; }
+    public required decimal SupplierPrice { get; set; }
 }
