5b2fa0492f439e34f833bb5bf0bf10c0dd4fb817
MuryloSaladino <murylosaladino@gmail.com>
2025-05-25 15:35:02 -0300

    feat: concise order item model

--- Almox.Domain/Entities/OrderItem.cs
+++ Almox.Domain/Entities/OrderItem.cs
@@ -2,13 +2,8 @@ namespace Almox.Domain.Entities;
 
 public class OrderItem
 {
-    public required Guid ItemId { get; set; }
-    public required Item Item { get; set; }
-
     public required Guid OrderId { get; set; }
-    public required Order Order { get; set; }
+    public required Guid ItemId { get; set; }
 
     public required int Quantity { get; set; }
-    public string? Observations { get; set; } = null;
-    public int FulfilledQuantity { get; set; } = 0;
 }
