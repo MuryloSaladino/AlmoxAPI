60d2b505a276f912ac47ccc9b5833a3b42accdd6
MuryloSaladino <murylosaladino@gmail.com>
2025-05-25 15:25:33 -0300

    feat: concise and simpler category entity

--- Almox.Domain/Entities/Category.cs
+++ Almox.Domain/Entities/Category.cs
@@ -3,8 +3,5 @@ namespace Almox.Domain.Entities;
 public class Category : BaseEntity
 {
     public required string Name { get; set; }
-    public required string Description { get; set; }
-    public required string Color { get; set; }
-    
     public List<Item> Items { get; } = [];
 }
