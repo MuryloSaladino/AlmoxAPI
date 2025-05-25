e514e65a621a53c1d4d56f45981c49fe4d34050a
MuryloSaladino <murylosaladino@gmail.com>
2025-05-25 16:09:38 -0300

    refact: exception data class name

--- Almox.Domain/Common/Exceptions/ExceptionData.cs
+++ Almox.Domain/Common/Exceptions/ExceptionData.cs
@@ -1,19 +1,19 @@
 namespace Almox.Domain.Common.Exceptions;
 
-public class ErrorSummary
+public class ExceptionData
 {
     public ExceptionCode ExceptionCode { get; set; }
     public string Message { get; set; }
     public string? Details { get; set; }
 
-    public ErrorSummary(ExceptionCode exceptionCode, string message, string? details = null)
+    public ExceptionData(ExceptionCode exceptionCode, string message, string? details = null)
     {
         ExceptionCode = exceptionCode;
         Message = message;
         Details = details;
     }
 
-    public ErrorSummary(Exception? ex = null)
+    public ExceptionData(Exception? ex = null)
     {
         ExceptionCode = ExceptionCode.InternalServerError;
         Message = ExceptionMessages.InternalServerError.Default;
