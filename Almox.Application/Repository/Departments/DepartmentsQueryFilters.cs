eb9f2ffceb22819de1a32886ff4e210be475d9a2
MuryloSaladino <murylosaladino@gmail.com>
2025-05-25 16:02:23 -0300

    feat: filters for departments

--- Almox.Application/Repository/Departments/DepartmentsQueryFilters.cs
+++
@@ -1,5 +0,0 @@
-namespace Almox.Application.Repository.Departments;
-
-public record DepartmentsQueryFilters(
-    string? Name
-);
--- Almox.Application/Repository/Departments/IDepartmentsRepository.cs
+++ Almox.Application/Repository/Departments/IDepartmentsRepository.cs
@@ -2,9 +2,9 @@ using Almox.Domain.Entities;
 
 namespace Almox.Application.Repository.Departments;
 
-public interface IDepartmentRepository : IBaseRepository<Department> 
-{
-    Task<List<Department>> GetWithFilters(
-        DepartmentsQueryFilters filters, CancellationToken cancellationToken);
-    Task<Department?> GetWithUsers(Guid id, CancellationToken cancellationToken);
-}
+public record DepartmentFilters(
+    string? Name
+);
+
+public interface IDepartmentRepository
+    : IBaseRepository<Department, DepartmentFilters>;
