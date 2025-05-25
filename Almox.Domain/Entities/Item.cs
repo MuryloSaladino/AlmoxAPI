b074692c90cc17ea49bcb8f34b75d920396af5c0
MuryloSaladino <murylosaladino@gmail.com>
2025-05-25 15:27:54 -0300

    feat: stock, price and description for item model

--- Almox.Domain/Entities/Item.cs
+++ Almox.Domain/Entities/Item.cs
@@ -3,7 +3,9 @@ namespace Almox.Domain.Entities;
 public class Item : BaseEntity
 {
     public required string Name { get; set; }
-    public required int Quantity { get; set; }
+    public required string Description { get; set; }
+    public required decimal Price { get; set; }
+    public required int Stock { get; set; }
     public string? ImageUrl { get; set; }
     
     public List<Category> Categories { get; } = [];
