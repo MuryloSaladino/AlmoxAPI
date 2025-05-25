f4f3395facf95fae78696b3de78fb9ba8f689c48
MuryloSaladino <murylosaladino@gmail.com>
2025-05-25 15:36:32 -0300

    feat: user role

--- Almox.Domain/Entities/User.cs
+++ Almox.Domain/Entities/User.cs
@@ -1,12 +1,14 @@
+using Almox.Domain.Common.Enums;
+
 namespace Almox.Domain.Entities;
 
 public class User : BaseEntity
 {
     public required Guid DepartmentId { get; set; }
-    public Department? Department { get; set; }
+    public required Department Department { get; set; }
 
     public required string Username { get; set; }
     public required string Email { get; set; }
     public required string Password { get; set; }
-    public bool IsAdmin { get; set; } = false;
+    public required UserRole Role { get; set; }
 }
