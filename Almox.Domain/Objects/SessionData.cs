66efe1fab3cb03d29bc797164832d8f858b7fcef
MuryloSaladino <murylosaladino@gmail.com>
2025-05-25 16:00:33 -0300

    feat: session data role and department id

--- Almox.Domain/Objects/SessionData.cs
+++ Almox.Domain/Objects/SessionData.cs
@@ -1,7 +1,10 @@
+using Almox.Domain.Common.Enums;
+
 namespace Almox.Domain.Objects;
 
 public class SessionData
 {
-    public required Guid UserId { get; set; }
-    public bool IsAdmin { get; set; } = false;
+    public required Guid UserId { get; init; }
+    public required Guid DepartmentId { get; init; }
+    public required UserRole Role { get; init; }
 }
