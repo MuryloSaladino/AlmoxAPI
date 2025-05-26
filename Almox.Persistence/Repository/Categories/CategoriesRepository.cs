37b831d4f64a9235fe6fb38da5a37327cab968e9
MuryloSaladino <murylosaladino@gmail.com>
2025-05-26 00:25:26 -0300

    feat: categories repository implementation for new application version

--- Almox.Persistence/Repository/Categories/CategoriesRepository.cs
+++ Almox.Persistence/Repository/Categories/CategoriesRepository.cs
@@ -1,9 +1,17 @@
 using Almox.Application.Repository.Categories;
 using Almox.Domain.Entities;
 using Almox.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Almox.Persistence.Repository.Categories;
 
 public class CategoriesRepository(
     AlmoxContext almoxContext
-) : BaseRepository<Category>(almoxContext), ICategoriesRepository {}
+) : BaseRepository<Category>(almoxContext), ICategoriesRepository
+{
+    public Task<List<Category>> GetAll(CategoryFilters filters, CancellationToken cancellationToken)
+        => context.Set<Category>()
+            .Where(c => c.DeletedAt == null)
+            .Where(c => filters.CategoryIds.Contains(c.Id))
+            .ToListAsync(cancellationToken);
+}
