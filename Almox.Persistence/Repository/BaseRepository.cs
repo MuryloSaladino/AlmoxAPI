76d7f8d50fd9730662a13957beaaa338f8f24a05
MuryloSaladino <murylosaladino@gmail.com>
2025-05-26 00:26:43 -0300

    refact: identation and reuse of variables

--- Almox.Persistence/Repository/BaseRepository.cs
+++ Almox.Persistence/Repository/BaseRepository.cs
@@ -9,7 +9,6 @@ public class BaseRepository<TEntity>(AlmoxContext AlmoxContext) : IBaseRepositor
     where TEntity : BaseEntity
 {
     protected readonly AlmoxContext context = AlmoxContext;
-    protected readonly DbSet<TEntity> dbSet = AlmoxContext.Set<TEntity>();
 
     public void Create(TEntity entity)
         => context.Add(entity);
@@ -21,14 +20,12 @@ public class BaseRepository<TEntity>(AlmoxContext AlmoxContext) : IBaseRepositor
     }
 
     public Task<TEntity?> Get(Guid id, CancellationToken cancellationToken)
-        => context
-            .Set<TEntity>()
+        => context.Set<TEntity>()
             .Where(entity => entity.DeletedAt == null)
             .FirstOrDefaultAsync(entity => entity.Id == id, cancellationToken);
 
     public Task<List<TEntity>> GetAll(CancellationToken cancellationToken)
-        => context
-            .Set<TEntity>()
+        => context.Set<TEntity>()
             .Where(entity => entity.DeletedAt == null)
             .ToListAsync(cancellationToken);
 
@@ -39,8 +36,9 @@ public class BaseRepository<TEntity>(AlmoxContext AlmoxContext) : IBaseRepositor
     }
     
     public Task<bool> Exists(Guid id, CancellationToken cancellationToken)
-        => dbSet.AnyAsync(e => 
-            EF.Property<Guid>(e, "Id") == id && 
-            EF.Property<Guid?>(e, "DeletedAt") == null, 
-        cancellationToken);
+        => context.Set<TEntity>()
+            .AnyAsync(e => 
+                EF.Property<Guid>(e, "Id") == id && 
+                EF.Property<Guid?>(e, "DeletedAt") == null, 
+            cancellationToken);
 }
