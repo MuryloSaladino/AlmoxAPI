dbb846342bdf99a5b37ffcc46865ff4e3343f875
MuryloSaladino <murylosaladino@gmail.com>
2025-05-26 19:59:31 -0300

    feat: departments filters and pagination implementation

--- Almox.Persistence/Repository/Departments/DepartmentsRepository.cs
+++ Almox.Persistence/Repository/Departments/DepartmentsRepository.cs
@@ -1,3 +1,4 @@
+using Almox.Application.Repository;
 using Almox.Application.Repository.Departments;
 using Almox.Domain.Entities;
 using Almox.Persistence.Context;
@@ -9,10 +10,25 @@ public class DepartmentsRepository(
     AlmoxContext context
 ) : BaseRepository<Department>(context), IDepartmentRepository
 {
-    public Task<List<Department>> GetAll(DepartmentFilters filters, CancellationToken cancellationToken)
-        => context.Set<Department>()
-            .Where(d => d.DeletedAt == null)
-            .Where(d => filters.Name == null || EF.Functions.ILike(d.Name, $"%{filters.Name}%"))
+    public async Task<PaginatedResult<Department>> GetAll(
+        DepartmentFilters filters, CancellationToken cancellationToken)
+    {
+        var query = context.Set<Department>()
+            .AsQueryable()
             .Include(d => d.Users)
+            .Where(e => e.DeletedAt == null);
+
+        if (filters.Name is string name)
+            query = query.Where(d => EF.Functions.ILike(d.Name, $"%{filters.Name}%"));
+
+        var count = await query.CountAsync(cancellationToken);
+        var maxPage = (int)Math.Ceiling(count / (double)filters.PageSize);
+
+        var results = await query
+            .Skip((filters.Page - 1) * filters.PageSize)
+            .Take(filters.PageSize)
             .ToListAsync(cancellationToken);
+
+        return new(filters.Page, filters.PageSize, maxPage, results);
+    }
 }
