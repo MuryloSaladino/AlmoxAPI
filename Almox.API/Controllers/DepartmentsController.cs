c64afa1e38f31cf864214cd3eae9ffc3a4bdc0df
MuryloSaladino <murylosaladino@gmail.com>
2025-05-26 01:54:26 -0300

    feat: departments controller with new application version

--- Almox.API/Controllers/DepartmentsController.cs
+++ Almox.API/Controllers/DepartmentsController.cs
@@ -1,8 +1,7 @@
 using Almox.API.Enums;
 using Almox.Application.Features.Departments.Create;
 using Almox.Application.Features.Departments.Delete;
-using Almox.Application.Features.Departments.Find;
-using Almox.Application.Features.Departments.FindById;
+using Almox.Application.Features.Departments.GetAll;
 using Almox.Application.Repository.Departments;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -25,24 +24,17 @@ public class DepartmentsController(IMediator mediator) : ControllerBase
     public async Task<ActionResult> Delete(
         [FromRoute] Guid departmentId, CancellationToken cancellationToken)
     {
-        await mediator.Send(new DeleteDepartmentRequest(departmentId), cancellationToken);
+        var request = new DeleteDepartmentRequest(departmentId);
+        await mediator.Send(request, cancellationToken);
         return NoContent();
     }
 
     [HttpGet]
-    public async Task<ActionResult<FindDepartmentsResponse>> Find(
-        [FromQuery] string? name, CancellationToken cancellationToken)
+    public async Task<ActionResult<GetAllDepartmentsResponse>> GetAll(
+        [FromQuery] DepartmentFilters filters, CancellationToken cancellationToken)
     {
-        var filters = new DepartmentsQueryFilters(name);
-        var response = await mediator.Send(new FindDepartmentsRequest(filters), cancellationToken);
-        return Ok(response);
-    }
-
-    [HttpGet, Route("{departmentId}")]
-    public async Task<ActionResult<FindDepartmentByIdResponse>> FindById(
-        [FromRoute] Guid departmentId, CancellationToken cancellationToken)
-    {
-        var response = await mediator.Send(new FindDepartmentByIdRequest(departmentId), cancellationToken);
+        var request = new GetAllDepartmentsRequest(filters);
+        var response = await mediator.Send(request, cancellationToken);
         return Ok(response);
     }
 }
