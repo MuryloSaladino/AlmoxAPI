766bc8d4917bacdfecdc0cdb69e31fecac6e65de
MuryloSaladino <murylosaladino@gmail.com>
2025-05-26 12:42:39 -0300

    refact: usage of base repository method to get categories with id list

--- Almox.Application/Repository/Categories/ICategoriesRepository.cs
+++ Almox.Application/Repository/Categories/ICategoriesRepository.cs
@@ -2,9 +2,5 @@ using Almox.Domain.Entities;
 
 namespace Almox.Application.Repository.Categories;
 
-public record CategoryFilters(
-    List<Guid> CategoryIds
-);
-
 public interface ICategoriesRepository
-    : IBaseRepository<Category, CategoryFilters>;
+    : IBaseRepository<Category>;
--- Almox.Persistence/Repository/Categories/CategoriesRepository.cs
+++ Almox.Persistence/Repository/Categories/CategoriesRepository.cs
@@ -1,17 +1,9 @@
 using Almox.Application.Repository.Categories;
 using Almox.Domain.Entities;
 using Almox.Persistence.Context;
-using Microsoft.EntityFrameworkCore;
 
 namespace Almox.Persistence.Repository.Categories;
 
 public class CategoriesRepository(
     AlmoxContext context
-) : BaseRepository<Category>(context), ICategoriesRepository
-{
-    public Task<List<Category>> GetAll(CategoryFilters filters, CancellationToken cancellationToken)
-        => context.Set<Category>()
-            .Where(c => c.DeletedAt == null)
-            .Where(c => filters.CategoryIds.Contains(c.Id))
-            .ToListAsync(cancellationToken);
-}
+) : BaseRepository<Category>(context), ICategoriesRepository;
