4da9ba552351cade20ecb2b7709e45832ca005ff
MuryloSaladino <murylosaladino@gmail.com>
2025-05-26 19:57:04 -0300

    feat: items paginated filters interfaces

--- Almox.Application/Repository/Items/IItemsRepository.cs
+++ Almox.Application/Repository/Items/IItemsRepository.cs
@@ -2,10 +2,11 @@ using Almox.Domain.Entities;
 
 namespace Almox.Application.Repository.Items;
 
-public record ItemFilters(
-    string? Name,
-    string? CategoryName
-);
+public record ItemFilters : PaginatedFilter
+{
+    public string? Name { get; init; }
+    public string? CategoryName { get; init; }
+}
 
 public interface IItemsRepository
     : IBaseRepository<Item, ItemFilters>;
