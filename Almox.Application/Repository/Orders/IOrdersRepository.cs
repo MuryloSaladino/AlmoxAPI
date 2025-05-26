f84aa17e7227db9e23d01cad420365c0fad30f8f
MuryloSaladino <murylosaladino@gmail.com>
2025-05-26 19:56:55 -0300

    feat: orders paginated filters interfaces

--- Almox.Application/Repository/Orders/IOrdersRepository.cs
+++ Almox.Application/Repository/Orders/IOrdersRepository.cs
@@ -3,13 +3,15 @@ using Almox.Domain.Entities;
 
 namespace Almox.Application.Repository.Orders;
 
-public record OrderFilters(
-    Guid? UserId,
-    OrderStatus? Status
-);
+public record OrderFilters : PaginatedFilter
+{
+    public OrderStatus? Status { get; init; }
+}
 
 public interface IOrdersRepository
     : IBaseRepository<Order, OrderFilters>
 {
     Task<Order?> GetUserCartOrder(Guid userId, CancellationToken cancellationToken);
+    Task<PaginatedResult<Order>> GetAllByUser(
+        Guid userId, OrderFilters filters, CancellationToken cancellationToken);
 }
