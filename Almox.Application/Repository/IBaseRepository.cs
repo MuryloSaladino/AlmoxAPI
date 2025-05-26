4b6893e85ffab6e6688e37b8550c75fae652dc61
MuryloSaladino <murylosaladino@gmail.com>
2025-05-26 19:56:21 -0300

    feat: pagination repository method

--- Almox.Application/Repository/IBaseRepository.cs
+++ Almox.Application/Repository/IBaseRepository.cs
@@ -2,8 +2,9 @@ using Almox.Domain.Entities;
 
 namespace Almox.Application.Repository;
 
-public interface IBaseRepository<TEntity>
+public interface IBaseRepository<TEntity, TFilters>
     where TEntity : BaseEntity
+    where TFilters : PaginatedFilter
 {
     void Create(TEntity entity);
     void Update(TEntity entity);
@@ -12,11 +13,9 @@ public interface IBaseRepository<TEntity>
     Task<TEntity?> Get(Guid id, CancellationToken cancellationToken);
     Task<List<TEntity>> GetAll(CancellationToken cancellationToken);
     Task<List<TEntity>> GetAll(IEnumerable<Guid> ids, CancellationToken cancellationToken);
+    Task<PaginatedResult<TEntity>> GetAll(TFilters filters, CancellationToken cancellationToken);
 }
 
-public interface IBaseRepository<TEntity, TFilters> 
-    : IBaseRepository<TEntity>
-        where TEntity : BaseEntity
-{
-    Task<List<TEntity>> GetAll(TFilters filters, CancellationToken cancellationToken);
-}
+public interface IBaseRepository<TEntity> 
+    : IBaseRepository<TEntity, PaginatedFilter>
+        where TEntity : BaseEntity;
