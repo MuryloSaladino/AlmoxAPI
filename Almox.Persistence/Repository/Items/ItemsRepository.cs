5ffaff53238f70fcbd32fe099f70bfa40df38fac
MuryloSaladino <murylosaladino@gmail.com>
2025-05-26 00:39:09 -0300

    feat: items repository implementation for new application version

--- Almox.Persistence/Repository/Items/ItemsRepository.cs
+++ Almox.Persistence/Repository/Items/ItemsRepository.cs
@@ -6,28 +6,23 @@ using Microsoft.EntityFrameworkCore;
 namespace Almox.Persistence.Repository.Items;
 
 public class ItemsRepository(
-    AlmoxContext almoxContext
-) : BaseRepository<Item>(almoxContext), IItemsRepository
+    AlmoxContext context
+) : BaseRepository<Item>(context), IItemsRepository
 {
-    public async Task<List<Item>> GetWithFilters(ItemsQueryFilters filters, CancellationToken cancellationToken)
+    public Task<List<Item>> GetAll(ItemFilters filters, CancellationToken cancellationToken)
     {
-        var query = dbSet
+        var query = context.Set<Item>()
             .Where(i => i.DeletedAt == null)
-            .Include(i => i.Categories)
             .AsQueryable();
 
         if (!string.IsNullOrWhiteSpace(filters.Name))
-        {
-            query = query.Where(i => EF.Functions.ILike(i.Name, $"%{filters.Name}%"));
-        }
+            query = query.Where(i =>
+                EF.Functions.ILike(i.Name, $"%{filters.Name}%"));
 
         if (!string.IsNullOrWhiteSpace(filters.CategoryName))
-        {
             query = query.Where(i =>
-                i.Categories.Any(c => EF.Functions.ILike(c.Name, $"%{filters.CategoryName}%"))
-            );
-        }
+                i.Categories.Any(c => EF.Functions.ILike(c.Name, $"%{filters.CategoryName}%")));
 
-        return await query.ToListAsync(cancellationToken);
+        return query.ToListAsync(cancellationToken);
     }
 }
