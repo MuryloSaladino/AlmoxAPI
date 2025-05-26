4b4912fe3aedd090f0929d1823a050a5496a9e09
MuryloSaladino <murylosaladino@gmail.com>
2025-05-25 23:54:21 -0300

    feat: new item entity mappings

---
+++ Almox.Persistence/TableMappings/Item.cs
@@ -0,0 +1,54 @@
+using Almox.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Almox.Persistence.TableMappings;
+
+public class ItemConfiguration : BaseEntityConfiguration<Item>
+{
+    public override void Configure(EntityTypeBuilder<Item> builder)
+    {
+        base.Configure(builder);
+
+        builder.ToTable("items");
+
+        builder.Property(e => e.Name)
+            .HasColumnName("name")
+            .HasColumnType("varchar(50)")
+            .IsRequired();
+
+        builder.Property(e => e.Description)
+            .HasColumnName("description")
+            .HasColumnType("varchar(255)")
+            .IsRequired();
+
+        builder.Property(e => e.Price)
+            .HasColumnName("price")
+            .HasColumnType("decimal(10,2)")
+            .IsRequired();
+
+        builder.Property(e => e.Stock)
+            .HasColumnName("stock")
+            .HasColumnType("int")
+            .IsRequired();
+
+        builder.Property(e => e.ImageUrl)
+            .HasColumnName("image_url")
+            .HasColumnType("varchar(255)");
+            
+        builder.HasMany(i => i.Categories)
+            .WithMany()
+            .UsingEntity<Dictionary<string, object>>("item_categories",
+                (ic) => ic.HasOne<Category>()
+                    .WithMany()
+                    .HasForeignKey("category_id"),
+                (ic) => ic.HasOne<Item>()
+                    .WithMany()
+                    .HasForeignKey("item_id"),
+                (ic) =>
+                {
+                    ic.ToTable("item_categories");
+                    ic.HasKey("item_id", "category_id");
+                });
+    }
+}
