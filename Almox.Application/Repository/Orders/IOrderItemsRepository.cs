78101b2310608c3635b99357f63d97884256f90b
MuryloSaladino <murylosaladino@gmail.com>
2025-05-25 21:13:15 -0300

    feat: get order item in repository

--- Almox.Application/Repository/Orders/IOrderItemsRepository.cs
+++ Almox.Application/Repository/Orders/IOrderItemsRepository.cs
@@ -6,4 +6,5 @@ public interface IOrderItemsRepository
 {
     void Create(OrderItem orderItem);
     void Delete(Guid itemId, Guid orderId);
+    Task<OrderItem?> Get(Guid itemId, Guid orderId, CancellationToken cancellationToken);
 }
