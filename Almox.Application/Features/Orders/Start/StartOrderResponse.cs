879862108d0784d876043ffa07a92dcec2cc368c
MuryloSaladino <murylosaladino@gmail.com>
2025-05-26 11:07:41 -0300

    feat: more concise response for start order

--- Almox.Application/Features/Orders/Start/StartOrderResponse.cs
+++ Almox.Application/Features/Orders/Start/StartOrderResponse.cs
@@ -4,13 +4,7 @@ using Almox.Domain.Entities;
 namespace Almox.Application.Features.Orders.Start;
 
 public sealed record StartOrderResponse(
-    Guid Id,
-    DateTime CreatedAt,
-    DateTime UpdatedAt,
-    DateTime? DeletedAt,
-    Guid UserId,
     OrderPriority Priority,
-    string Tracking,
     string? Observations,
     List<OrderItem> OrderItems
 );
