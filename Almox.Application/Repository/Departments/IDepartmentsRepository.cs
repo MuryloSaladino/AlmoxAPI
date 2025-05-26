96412218ebb72561bdbc51be86f09795d27afab1
MuryloSaladino <murylosaladino@gmail.com>
2025-05-26 19:57:14 -0300

    feat: departments paginated filters interfaces

--- Almox.Application/Repository/Departments/IDepartmentsRepository.cs
+++ Almox.Application/Repository/Departments/IDepartmentsRepository.cs
@@ -2,9 +2,10 @@ using Almox.Domain.Entities;
 
 namespace Almox.Application.Repository.Departments;
 
-public record DepartmentFilters(
-    string? Name
-);
+public record DepartmentFilters : PaginatedFilter
+{
+    public string? Name { get; init; }
+}
 
 public interface IDepartmentRepository
     : IBaseRepository<Department, DepartmentFilters>;
