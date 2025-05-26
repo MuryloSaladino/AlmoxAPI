13a4487d71f79a732a2ced786156f3fd7747eb45
MuryloSaladino <murylosaladino@gmail.com>
2025-05-26 02:09:22 -0300

    feat: users controller with new application version

--- Almox.API/Controllers/UsersControllers.cs
+++ Almox.API/Controllers/UsersControllers.cs
@@ -1,10 +1,9 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Almox.Application.Features.Users.Register;
-using Almox.Application.Features.Users.FindById;
-using Almox.Application.Features.Users.Promote;
+using Almox.Application.Features.Users.Get;
 using Almox.API.Enums;
-using Almox.Application.Features.Users.Find;
+using Almox.Application.Features.Users.GetAll;
 using Almox.Application.Repository.Users;
 
 namespace Almox.API.Controllers;
@@ -22,29 +21,20 @@ public class UsersController(IMediator mediator) : ControllerBase
     }
 
     [HttpGet, Route("{userId}")]
-    public async Task<ActionResult<FindUserByIdResponse>> FindById(
+    public async Task<ActionResult<GetUserResponse>> Get(
         [FromRoute] Guid userId, CancellationToken cancellationToken)
     {
-        var response = await mediator.Send(new FindUserByIdRequest(userId), cancellationToken);
+        var request = new GetUserRequest(userId);
+        var response = await mediator.Send(request, cancellationToken);
         return Ok(response);
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<FindUsersResponse>>> Find(
-        [FromQuery] string? username,
-        [FromQuery] string? email,
-        CancellationToken cancellationToken)
+    public async Task<ActionResult<List<GetAllUsersResponse>>> GetAll(
+        [FromQuery] UserFilters filters, CancellationToken cancellationToken)
     {
-        var filters = new UsersQueryFilters(username, email);
-        var response = await mediator.Send(new FindUsersRequest(filters), cancellationToken);
-        return Ok(response);
-    }
-
-    [HttpPatch, Route("promote/{userId}")]
-    public async Task<ActionResult<PromoteUserResponse>> Promote(
-        [FromRoute] Guid userId, CancellationToken cancellationToken)
-    {
-        var response = await mediator.Send(new PromoteUserRequest(userId), cancellationToken);
+        var request = new GetAllUsersRequest(filters);
+        var response = await mediator.Send(request, cancellationToken);
         return Ok(response);
     }
 }
