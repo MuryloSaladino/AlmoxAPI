9242547f261c8ca6b1e0982f1621b2081eb064fa
MuryloSaladino <murylosaladino@gmail.com>
2025-05-26 01:36:17 -0300

    feat: staff and admin verifications in request session

--- Almox.API/Security/RequestSession.cs
+++ Almox.API/Security/RequestSession.cs
@@ -1,6 +1,7 @@
 using Almox.Application.Common.Exceptions;
 using Almox.Application.Common.Session;
-using Almox.Domain.Common.Messages;
+using Almox.Domain.Common.Enums;
+using Almox.Domain.Common.Exceptions;
 using Almox.Domain.Objects;
 
 namespace Almox.API.Security;
@@ -8,10 +9,30 @@ namespace Almox.API.Security;
 public class RequestSession(SessionData? session = null) : IRequestSession
 {
     public SessionData? Session { get; set; } = session;
+    
+    public void SetSession(SessionData? session)
+        => Session = session;
 
     public SessionData GetSessionOrThrow()
         => Session ?? throw AppException.Unauthorized(ExceptionMessages.Unauthorized.Session);
 
-    public void SetSession(SessionData? session)
-        => Session = session;
+    public SessionData GetStaffSessionOrThrow()
+    {
+        var session = GetSessionOrThrow();
+
+        if (session.Role.Equals(UserRole.Employee))
+            throw AppException.Forbidden(ExceptionMessages.Forbidden.Role);
+
+        return session;
+    }
+
+    public SessionData GetAdminSessionOrThrow()
+    {
+        var session = GetSessionOrThrow();
+
+        if (!session.Role.Equals(UserRole.Admin))
+            throw AppException.Forbidden(ExceptionMessages.Forbidden.Role);
+
+        return session;
+    }
 }
