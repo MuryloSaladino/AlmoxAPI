59ba6f03df3887ece1a8e51d3b7b591b1cd54c9c
MuryloSaladino <murylosaladino@gmail.com>
2025-05-26 19:56:47 -0300

    feat: users paginated filters interfaces

--- Almox.Application/Repository/Users/IUsersRepository.cs
+++ Almox.Application/Repository/Users/IUsersRepository.cs
@@ -2,12 +2,14 @@ using Almox.Domain.Entities;
 
 namespace Almox.Application.Repository.Users;
 
-public record UserFilters(
-    string? Username,
-    string? Email
-);
+public record UserFilters : PaginatedFilter
+{
+    public string? Username { get; init; }
+    public string? Email { get; init; }
+}
 
-public interface IUsersRepository : IBaseRepository<User, UserFilters>
+public interface IUsersRepository
+    : IBaseRepository<User, UserFilters>
 {
     Task<User?> GetByUsernameOrEmail(string search, CancellationToken cancellationToken);
 }
