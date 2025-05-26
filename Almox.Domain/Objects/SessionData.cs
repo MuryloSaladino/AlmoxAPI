4e9d66c9c50a53deaa34ddf2af7692ac5bb2310d
MuryloSaladino <murylosaladino@gmail.com>
2025-05-26 01:30:39 -0300

    remove: departmentId from session data object

--- Almox.Domain/Objects/SessionData.cs
+++ Almox.Domain/Objects/SessionData.cs
@@ -5,6 +5,5 @@ namespace Almox.Domain.Objects;
 public class SessionData
 {
     public required Guid UserId { get; init; }
-    public required Guid DepartmentId { get; init; }
     public required UserRole Role { get; init; }
 }
