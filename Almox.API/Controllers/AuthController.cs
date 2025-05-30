09e0bc3f2c3bbaf20d3401a715985f6cd30f2d24
MuryloSaladino <murylosaladino@gmail.com>
2025-05-30 16:08:19 -0300

    feat: refresh token route

--- Almox.API/Controllers/AuthController.cs
+++ Almox.API/Controllers/AuthController.cs
@@ -4,6 +4,10 @@ using Almox.Application.Features.Auth.Login;
 using Almox.API.Constants;
 using Almox.Application.Features.Auth.Logout;
 using Almox.API.Pipeline.Filters;
+using Almox.Application.Features.Auth.RefreshTokens;
+using Almox.Application.Common.Exceptions;
+using Almox.Domain.Common.Exceptions;
+using Almox.Application.Features.Users.Get;
 
 namespace Almox.API.Controllers;
 
@@ -11,26 +15,25 @@ namespace Almox.API.Controllers;
 public class AuthController(IMediator mediator) : ControllerBase
 {
     [HttpPost, Route("login")]
-    public async Task<ActionResult> Login(
+    public async Task<ActionResult<GetUserResponse>> Login(
         LoginRequest request, CancellationToken cancellationToken)
     {
         var response = await mediator.Send(request, cancellationToken);
+        SetTokenCookies(response.AccessToken, response.RefreshToken);
+        return Ok(new { user = response.User });
+    }
 
-        Response.Cookies.Append(Cookies.AccessToken, response.AccessToken, new CookieOptions
-        {
-            HttpOnly = true,
-            Secure = Environment.GetEnvironmentVariable("ENV_MODE") == "prod",
-            SameSite = SameSiteMode.Strict,
-            Expires = response.ExpiresAt,
-        });
-        Response.Cookies.Append(Cookies.RefreshToken, response.RefreshToken, new CookieOptions
-        {
-            HttpOnly = true,
-            Secure = Environment.GetEnvironmentVariable("ENV_MODE") == "prod",
-            SameSite = SameSiteMode.Strict,
-            Expires = DateTime.UtcNow.AddDays(15),
-        });
+    [HttpPost, Route("refresh/{userId}")]
+    public async Task<ActionResult> RefreshTokens(
+        [FromRoute] Guid userId, CancellationToken cancellationToken)
+    {
+        if (!Request.Cookies.TryGetValue(Cookies.RefreshToken, out string? refreshToken))
+            throw AppException.Unauthorized(ExceptionMessages.Unauthorized.RefreshToken);
 
+        var request = new RefreshTokensRequest(userId, refreshToken);
+        var response = await mediator.Send(request, cancellationToken);
+
+        SetTokenCookies(response.AccessToken, response.RefreshToken);
         return Ok();
     }
 
@@ -39,15 +42,35 @@ public class AuthController(IMediator mediator) : ControllerBase
         [FromQuery] LogoutRequest request, CancellationToken cancellationToken)
     {
         await mediator.Send(request, cancellationToken);
+        DeleteTokenCookies();
+        return NoContent();
+    }
 
+    private void SetTokenCookies(string accessToken, string refreshToken)
+    {
+        Response.Cookies.Append(Cookies.AccessToken, accessToken, new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = Environment.GetEnvironmentVariable("ENV_MODE") == "prod",
+            SameSite = SameSiteMode.Strict,
+            Expires = DateTime.UtcNow.AddMinutes(15),
+        });
+        Response.Cookies.Append(Cookies.RefreshToken, refreshToken, new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = Environment.GetEnvironmentVariable("ENV_MODE") == "prod",
+            SameSite = SameSiteMode.Strict,
+            Expires = DateTime.UtcNow.AddDays(30),
+        });
+    }
+
+    private void DeleteTokenCookies()
+    {
         var cookieOptions = new CookieOptions
         {
             Expires = DateTime.UtcNow.AddDays(-1),
         };
-        
         Response.Cookies.Append(Cookies.AccessToken, string.Empty, cookieOptions);
         Response.Cookies.Append(Cookies.RefreshToken, string.Empty, cookieOptions);
-
-        return NoContent();
     }
 }
