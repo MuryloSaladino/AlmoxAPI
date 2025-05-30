c2c675d86e97166ee6b663bd902c72494d2cf1d1
MuryloSaladino <murylosaladino@gmail.com>
2025-05-30 11:44:28 -0300

    chore: moved cors config to different package

---
+++ Almox.API/Security/Config/CorsPolicy.cs
@@ -0,0 +1,15 @@
+namespace Almox.API.Security.Config;
+
+public static class CorsPolicyExtensions
+{
+    public static void ConfigureCorsPolicy(this IServiceCollection services)
+    {
+        services.AddCors(opt =>
+            opt.AddDefaultPolicy(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+            )
+        );
+    }
+}
