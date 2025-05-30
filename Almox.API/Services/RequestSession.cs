f4364491397b7069e60f58d4563a6a9e62ff2f12
MuryloSaladino <murylosaladino@gmail.com>
2025-05-30 11:44:12 -0300

    chore: moved request session to different package

---
+++ Almox.API/Services/RequestSession.cs
@@ -0,0 +1,38 @@
+using Almox.Application.Common.Exceptions;
+using Almox.Application.Common.Session;
+using Almox.Domain.Common.Enums;
+using Almox.Domain.Common.Exceptions;
+using Almox.Domain.Objects;
+
+namespace Almox.API.Services;
+
+public class RequestSession(SessionData? session = null) : IRequestSession
+{
+    public SessionData? Session { get; set; } = session;
+    
+    public void SetSession(SessionData? session)
+        => Session = session;
+
+    public SessionData GetSessionOrThrow()
+        => Session ?? throw AppException.Unauthorized(ExceptionMessages.Unauthorized.Session);
+
+    public SessionData GetStaffSessionOrThrow()
+    {
+        var session = GetSessionOrThrow();
+
+        if (session.Role.Equals(UserRole.Employee))
+            throw AppException.Forbidden(ExceptionMessages.Forbidden.Role);
+
+        return session;
+    }
+
+    public SessionData GetAdminSessionOrThrow()
+    {
+        var session = GetSessionOrThrow();
+
+        if (!session.Role.Equals(UserRole.Admin))
+            throw AppException.Forbidden(ExceptionMessages.Forbidden.Role);
+
+        return session;
+    }
+}
