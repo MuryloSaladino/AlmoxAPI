3be65617472ed61bbfe1c3bae84e3496ee134bf6
MuryloSaladino <murylosaladino@gmail.com>
2025-05-31 11:46:58 -0300

    feat: app constants for route and keys

---
+++ src/app/core/constants/app-routes.ts
@@ -0,0 +1,14 @@
+export enum AppRoutes {
+	ROOT = "",
+	NOT_FOUND = "/not-found",
+
+	LOGIN = "/login",
+
+	INVENTORY = "/inventory",
+	DELIVERIES = "/deliveries",
+
+	DASHBOARD = "/dashboard",
+	CATALOG = "/catalog",
+	CART = "/cart",
+	MY_ORDERS = "/my-orders",
+}
