33a42160b3096c245c1a3df3686f18895f5ee524
MuryloSaladino <murylosaladino@gmail.com>
2025-06-01 17:21:55 -0300

    feat: focused api types

--- src/app/core/services/api/api.interfaces.ts
+++ src/app/core/services/api/api.interfaces.ts
@@ -1,14 +1,3 @@
-import { User } from "../../entities/user.entity";
-
-export interface LoginRequest {
-    userIdentifier: string;
-    password: string;
-}
-
-export interface LoginResponse {
-    user: User;
-}
-
 export interface Paginated<T> {
     page: number;
     pageSize: number;
