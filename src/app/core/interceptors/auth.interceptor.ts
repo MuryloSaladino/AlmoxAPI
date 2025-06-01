2edd6c7478c6fdfc26bd970dee8a91f742192296
MuryloSaladino <murylosaladino@gmail.com>
2025-06-01 17:22:46 -0300

    feat: refresh interceptor redirecting

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,11 +1,14 @@
 import { inject } from '@angular/core';
 import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { catchError, switchMap, throwError } from 'rxjs';
-import { ApiService } from '../services/api/api.service';
+import { AuthService } from '../services/auth/auth.service';
+import { Router } from '@angular/router';
+import { AppRoutes } from '../constants/app-routes';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
-	const api = inject(ApiService);
+	const auth = inject(AuthService);
+	const router = inject(Router);
 	let isRefreshing = false;
 
 	return next(req).pipe(
@@ -13,15 +16,15 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
 			if (error.status === 401 && !isRefreshing) {
 				isRefreshing = true;
 
-				return api.refresh().pipe(
+				return auth.refresh().pipe(
 					switchMap(() => next(req)),
 					catchError((refreshError) => {
-						api.user.set(null);
+						auth.userSubject.next(null);
+						router.navigate([AppRoutes.LOGIN])
 						return throwError(() => refreshError);
 					})
 				);
 			}
-
 			return throwError(() => error);
 		})
 	);
