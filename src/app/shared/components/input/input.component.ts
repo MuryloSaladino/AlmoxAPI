a6fb012a8a2e66739933188907049550065fd875
MuryloSaladino <murylosaladino@gmail.com>
2025-06-01 21:00:24 -0300

    feat: input component

---
+++ src/app/shared/components/input/input.component.ts
@@ -0,0 +1,53 @@
+import { Component, Input, forwardRef } from "@angular/core";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from "@angular/forms";
+import { v4 as uuid } from "uuid";
+
+@Component({
+    selector: "app-input",
+    templateUrl: "./input.component.html",
+    styleUrl: "./input.component.css",
+    standalone: true,
+    imports: [ReactiveFormsModule],
+    providers: [{
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(() => InputComponent),
+        multi: true
+    }]
+})
+export class InputComponent implements ControlValueAccessor {
+    @Input() id: string = uuid();
+    @Input() label: string = "";
+    @Input() helperText: string = "";
+    @Input() type: string = "text";
+    @Input() disabled: boolean = false;
+    @Input() className: string = "";
+    @Input() error: boolean = false;
+
+    value: string = "";
+
+    onChange = (_: any) => { };
+    onTouched = () => { };
+
+    writeValue(value: any): void {
+        this.value = value || "";
+    }
+
+    registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any): void {
+        this.onTouched = fn;
+    }
+
+    setDisabledState?(isDisabled: boolean): void {
+        this.disabled = isDisabled;
+    }
+
+    handleInput(event: Event) {
+        const value = (event.target as HTMLInputElement).value;
+        this.value = value;
+        this.onChange(value);
+        this.onTouched();
+    }
+}
