69430ef9a5f40772e4488a1e3d04fbac533825c2
MuryloSaladino <murylosaladino@gmail.com>
2025-06-01 21:16:12 -0300

    feat: button variants classes

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -6,15 +6,14 @@ import { IconComponent } from "../icon/icon.component";
 	templateUrl: "./button.component.html",
 	styleUrl: "./button.component.css",
 	standalone: true,
-	imports: [
-		IconComponent,
-	]
+	imports: [IconComponent]
 })
 export class ButtonComponent {
 	@Input() type: "button" | "submit" | "reset" = "button";
 	@Input() disabled = false;
 	@Input("class") extraClasses = "";
 
+	@Input() variant: "primary" | "white" = "primary"
 	@Input() rightIcon?: string;
 	@Input() leftIcon?: string;
     @Output() click = new EventEmitter<Event>();
