43d3cc7ce94a28e0a6a9019f5f13ee1a874a168e
MuryloSaladino <murylosaladino@gmail.com>
2025-06-02 22:49:10 -0300

    feat: simplyfied auth guard with tryCookiesAuth

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,15 +1,9 @@
 import { inject } from "@angular/core";
 import { AuthService } from "../services/auth/auth.service";
-import { map } from "rxjs";
 
-export const authGuard = () => {
+export const authGuard = async () => {
 
 	const auth = inject(AuthService);
 
-	if(auth.userSubject.value)
-		return true;
-
-	return auth.tryCookiesAuth().pipe(map(() => {
-		return Boolean(auth.userSubject.value);
-	}));
+	return Boolean(auth.user()) || await auth.tryCookiesAuth()
 };
