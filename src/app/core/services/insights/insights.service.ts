5eceb069ebb80eba1dd48025a0ee5c332f634b86
MuryloSaladino <murylosaladino@gmail.com>
2025-06-04 23:44:21 -0300

    feat: inventory insights endpoint service

--- src/app/core/services/insights/insights.service.ts
+++ src/app/core/services/insights/insights.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from "@angular/core";
 import { http } from "../../http";
-import { AdminInsights } from "./insights.types";
+import { AdminInsights, InventoryInsights } from "./insights.types";
 
 @Injectable({ providedIn: "root" })
 export class InsightsService {
 
 	async admin() {
 		return await http.get<AdminInsights>("/insights/admin");
+	}
+
+	async inventory() {
+		return await http.get<InventoryInsights>("/insights/inventory");
 	}
 }
