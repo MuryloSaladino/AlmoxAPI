45e74a1b6d2c64d3dc7081a291778df8c176695a
MuryloSaladino <murylosaladino@gmail.com>
2025-06-06 00:37:45 -0300

    feat: category management component

---
+++ src/app/pages/inventory/components/category-management/category-management.component.ts
@@ -0,0 +1,46 @@
+import { Component, inject, signal } from "@angular/core";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { CardComponent } from "../../../../shared/components/card/card.component";
+import { InputComponent } from "../../../../shared/components/input/input.component";
+import { ButtonComponent } from "../../../../shared/components/button/button.component";
+import { ServerTableComponent } from "../../../../shared/components/server-table/server-table.component";
+import { CategoryService } from "../../../../core/services/category/category.service";
+import { ServerTableColumn } from "../../../../shared/components/server-table/server-table.types";
+import { Category } from "../../../../core/types/entities/category.entity";
+
+@Component({
+	selector: "category-management",
+	templateUrl: "./category-management.component.html",
+ 	imports: [
+		CardComponent,
+		InputComponent,
+		ButtonComponent,
+		ReactiveFormsModule,
+		ServerTableComponent,
+	]
+})
+export class CategoryManagementComponent {
+
+	readonly categoryService = inject(CategoryService);
+	readonly loading = signal(false);
+	readonly page = signal(1);
+	readonly form: FormGroup;
+
+	constructor(private fb: FormBuilder) {
+		this.form = this.fb.group({
+			name: ['', [Validators.required, Validators.minLength(3)]],
+		});
+	}
+
+	async submit() {
+		this.loading.set(true);
+		await this.categoryService.create(this.form.value);
+		this.loading.set(false);
+		this.form.reset();
+	}
+
+	readonly columns: ServerTableColumn<Category>[] = [
+		{ label: "Name", path: "name" },
+		{ label: "Data de Criação", path: "createdAt", pipe: "date" },
+	]
+}
