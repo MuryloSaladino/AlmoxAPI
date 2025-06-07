ada2577f3b172d8b5de657ada2ecbb443b4296f8
MuryloSaladino <murylosaladino@gmail.com>
2025-06-07 19:48:56 -0300

    feat: cart managing assets

---
+++ src/app/core/services/cart/cart.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { StorageKeys } from "../../constants/storage-keys";
+import { OrderCreation, OrderItem } from "../../types/entities/order.entity";
+
+@Injectable({ providedIn: "root" })
+export class CartService {
+
+	getCart(): OrderCreation["orderedItems"] {
+		let cart = localStorage.getItem(StorageKeys.CART);
+
+		if(!cart) {
+			localStorage.setItem(StorageKeys.CART, "[]");
+			return [];
+		}
+
+		return JSON.parse(cart);
+	}
+
+	addToCart(itemId: string, quantity: number) {
+		const cart = this.getCart();
+		cart.push({ itemId, quantity });
+		localStorage.setItem(StorageKeys.CART, JSON.stringify(cart));
+	}
+
+	removeItem(itemId: string) {
+		const cart = this.getCart().filter(x => x.itemId != itemId);
+		localStorage.setItem(StorageKeys.CART, JSON.stringify(cart));
+	}
+}
