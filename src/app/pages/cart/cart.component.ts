1ab14c0d12ca6b3f83ff3e8fd2324c4771468caf
MuryloSaladino <murylosaladino@gmail.com>
2025-06-08 23:53:10 -0300

    feat: cart page with dynamic management

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,11 +1,49 @@
-import { Component } from "@angular/core";
+import { Component, computed, effect, inject, signal } from "@angular/core";
+import { CardComponent } from "../../shared/components/card/card.component";
+import { CartService } from "../../core/services/cart/cart.service";
+import { CartItemComponent } from "./components/cart-item/cart-item.component";
+import { OrderSummaryComponent } from "./components/order-summary/order-summary.component";
+import { RouterModule } from "@angular/router";
+import { AppRoutes } from "../../core/constants/app-routes";
+import { ButtonComponent } from "../../shared/components/button/button.component";
+import { ItemService } from "../../core/services/item/item.service";
+import { Item } from "../../core/types/entities/items.types";
 
 @Component({
 	selector: "orders",
 	templateUrl: "./cart.component.html",
 	standalone: true,
-	imports: [],
+	imports: [
+		CardComponent,
+		CartItemComponent,
+		OrderSummaryComponent,
+		RouterModule,
+		ButtonComponent
+	],
 })
 export class CartComponent {
 
+	readonly cartService = inject(CartService);
+	readonly itemService = inject(ItemService);
+	readonly catalog = AppRoutes.CATALOG;
+	readonly cart = signal(this.cartService.getCart());
+
+	readonly items = signal<Item[]>([]);
+
+	constructor() {
+		effect(async () => this.items.set(await Promise.all(
+			Object.keys(this.cart()).map(async (itemId) => await this.itemService.get(itemId))
+		)));
+	}
+
+	removeItem(itemId: string) {
+		return () => {
+			this.cartService.removeItem(itemId);
+			this.cart.set(this.cartService.getCart());
+		}
+	}
+
+	getTotal() {
+		return this.items().reduce((acc, curr) => (curr.price * this.cart()[curr.id]) + acc, 0)
+	}
 }
