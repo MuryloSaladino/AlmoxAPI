b29924e19d27374adbf25c0ed11f45a1fbaafd48
MuryloSaladino <murylosaladino@gmail.com>
2025-06-08 23:51:21 -0300

    feat: cart structure as dictionary for performance

--- src/app/core/services/cart/cart.service.ts
+++ src/app/core/services/cart/cart.service.ts
@@ -1,30 +1,30 @@
 import { Injectable } from "@angular/core";
 import { StorageKeys } from "../../constants/storage-keys";
-import { OrderCreation, OrderItem } from "../../types/entities/order.entity";
 
 @Injectable({ providedIn: "root" })
 export class CartService {
 
-	getCart(): OrderCreation["orderedItems"] {
+	getCart(): { [key: string]: number } {
 		let cart = localStorage.getItem(StorageKeys.CART);
 
 		if(!cart) {
-			localStorage.setItem(StorageKeys.CART, "[]");
-			return [];
+			localStorage.setItem(StorageKeys.CART, "{}");
+			return {};
 		}
 
 		return JSON.parse(cart);
 	}
 
 	addToCart(itemId: string) {
-		const quantity = Number(prompt("How many would you like?")) || 1
+		const quantity = Number(prompt("How many would you like?", "1"))
 		const cart = this.getCart();
-		cart.push({ itemId, quantity });
+		cart[itemId] = quantity;
 		localStorage.setItem(StorageKeys.CART, JSON.stringify(cart));
 	}
 
 	removeItem(itemId: string) {
-		const cart = this.getCart().filter(x => x.itemId != itemId);
+		const cart = this.getCart();
+		delete cart[itemId];
 		localStorage.setItem(StorageKeys.CART, JSON.stringify(cart));
 	}
 }
